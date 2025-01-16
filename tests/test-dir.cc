5a92a8752c869e94aca1edc1a95196adfdcb74dc
Leo Broukhis <leobru@users.noreply.github.com>
2025-01-15 17:27:18 -0800

    Update test-dir.cc
    Added cleardir, rmdir.

--- tests/test-dir.cc
+++ tests/test-dir.cc
@@ -43,3 +43,63 @@ TEST(mars, dir)
     EXPECT_EQ(mars.putd(2, 0, 0), Mars::ERR_SUCCESS);  // 2 is not there
     EXPECT_EQ(mars.getd(10, 0, 0), Mars::ERR_SUCCESS); // but 10 is
 }
+
+// Before calling this rmdir, the directory to be removed must be the current directory
+static Mars::Error rmdir(Mars & mars, uint64_t dir) {
+    mars.data[Mars::BDVECT] = 077770;
+    mars.key = 0;
+    mars.data[Mars::BDVECT+2] = dir;
+    mars.data[077770] = Mars::mcprog(Mars::OP_LAST, Mars::OP_NOMATCH,
+                                     Mars::OP_COND, Mars::OP_FREE, Mars::OP_DELKEY, Mars::OP_LOOP,
+                                     Mars::OP_DELKEY, Mars::OP_CHAIN);
+    mars.data[077771] = Mars::mcprog(Mars::OP_ASSIGN, Mars::Op(010), Mars::Op(2), Mars::OP_CHAIN);
+    mars.data[077772] = (Mars::mcprog(Mars::OP_ASSIGN, Mars::Op(035), Mars::Op(014),
+                                      Mars::OP_SETMETA,
+                                      Mars::OP_FIND, Mars::OP_FREE, Mars::OP_DELKEY));
+    return mars.eval(Mars::mcprog(Mars::OP_BEGIN, Mars::OP_ASSIGN,
+                                  Mars::Op(014), Mars::Op(035), Mars::OP_CHAIN));
+}
+
+TEST(mars, cleardir)
+{
+    Mars mars(false);
+    std::string result;
+    mars.InitDB(0, 0, 20);
+    mars.SetDB(0, 0, 20);
+    mars.root();
+    int before = mars.avail();
+    EXPECT_EQ(mkdir(mars, 12345), Mars::ERR_SUCCESS);
+    EXPECT_EQ(chdir(mars, 12345), Mars::ERR_SUCCESS);
+    // Results in secondary metablocks for the directory
+    for (int i = 1; i < 1000; ++i) 
+        ASSERT_EQ(mars.putd(i, 0, 0), Mars::ERR_SUCCESS);
+    // This will not discard secondary metablocks
+    EXPECT_EQ(mars.cleard(false), Mars::ERR_SUCCESS);
+    EXPECT_EQ(dotdot(mars),  Mars::ERR_SUCCESS);
+    EXPECT_EQ(mars.deld(12345), Mars::ERR_SUCCESS);
+    int after = mars.avail();
+    // There will be a memory leak of 10 words
+    EXPECT_EQ(before - after, 10);
+}
+
+TEST(mars, rmdir)
+{
+    Mars mars(false);
+    std::string result;
+    mars.InitDB(0, 0, 20);
+    mars.SetDB(0, 0, 20);
+    mars.root();
+    std::cerr << before << '\n';
+    EXPECT_EQ(mkdir(mars, 12345), Mars::ERR_SUCCESS);
+    EXPECT_EQ(chdir(mars, 12345), Mars::ERR_SUCCESS);
+    // Results in secondary metablocks for the directory
+    for (int i = 1; i < 1000; ++i) 
+        ASSERT_EQ(mars.putd(i, 0, 0), Mars::ERR_SUCCESS);
+    // This should discard all secondary metablocks
+    EXPECT_EQ(rmdir(mars, 12345), Mars::ERR_SUCCESS);
+    int after = mars.avail();
+    // Therefore reclaiming all used memory
+    EXPECT_EQ(before, after);
+    // Checking that the directory had been deleted, and that the DB is not corrupt.
+    ASSERT_EQ(mars.putd(12345, 0, 0), Mars::ERR_SUCCESS);
+}
